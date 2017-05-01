84a063a2ee977a5818615a3abe5da64c948906d1
jonah-hooper <jonah.graham.hooper@gmail.com>
2017-05-01 21:41:46 +0200

    Added a code viewer for parser with an example

--- src/viewer.js
+++ src/viewer.js
@@ -2,23 +2,61 @@ const Parser = require('./parser')
 const Evaluator = require('./evaluator')
 const codemirror = require('codemirror')
 
-function init() {
-  initParsers()
-}
 
-function initParsers() {
-  let parsers = document.getElementsByTagName('textarea')
-  for (let child of parsers) {
+function initEditable(pane) {
+  let editor = pane.getElementsByClassName('editor').item(0)
+  let viewer = pane.getElementsByClassName('viewer').item(0)
+  let button = pane.getElementsByClassName('evaluate-button').item(0)
+  return {
+    editor: editor,
+    viewer: viewer,
+    pane: pane,
+    button: button
   }
 }
 
-function initEvaluators() {
+const editableStyle = `
+  border: solid 1px rgb(200, 200, 200);
+  border-radius: 3px;
+`
+
+function renderContent(editable, parserCallback) {
+  var code = editable.editor.textContent
+  try {
+    let ast = parserCallback(code)
+    let astText = JSON.stringify(ast, null, 2)
+    editable.viewer.textContent = astText
+    editable.viewer.style.cssText = editableStyle
+  } catch (parseError) {
+    console.log(parseError)
+    editable.viewer.textContent = parseError.toString()
+    editable.viewer.style.cssText = `
+      border: solid 2px rgb(230, 70, 70);
+      border-radius: 3px;
+      padding:1px;
+    `
+  }
 }
 
-function initParserViewer(initialString, parser) {
+
+function setStyle(editable) {
+  //pane.style.cssText = `display: flex;`
+  editable.editor.style.cssText = editableStyle
+  editable.viewer.style.cssText = editableStyle
 }
 
-function initEvaluatorViewer(initalString, initalEnv, evaluator) {
+function addAction(editable, parserCallback) {
+  editable.button.onclick = () => {
+    renderContent(editable, parserCallback)
+  }
 }
 
-module.exports.init = init
+function initParser(parserEditorID, parserCallback) {
+  let pane = document.getElementById(parserEditorID)
+  let editable = initEditable(pane)
+  renderContent(editable, parserCallback)
+  addAction(editable, parserCallback)
+  setStyle(editable)
+}
+
+module.exports.initParser = initParser
